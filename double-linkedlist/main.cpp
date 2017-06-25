a7e7ac90ae52b66e3da39f1b104ac3816ef217a5
danghai <goldsea5191@gmail.com>
2017-06-25 13:51:08 -0700

    double linkedlist

---
+++ double-linkedlist/main.cpp
@@ -0,0 +1,82 @@
+/*
+  Name : Hai Dang Hoang
+  Email: goldsea5191@gmail.com
+  Implement Double Linked list 
+*/
+#include "doublell.h"
+int main()
+{
+    int choice, element, position;
+    doublell dl;
+    while (1)
+    {
+        cout<<endl<<"----------------------------"<<endl;
+        cout<<endl<<"Operations on Doubly linked list"<<endl;
+        cout<<endl<<"----------------------------"<<endl;
+        cout<<"1.Create Node"<<endl;
+        cout<<"2.Add at begining"<<endl;
+        cout<<"3.Add after position"<<endl;
+        cout<<"4.Delete"<<endl;
+        cout<<"5.Display"<<endl;
+        cout<<"6.Count"<<endl;
+        cout<<"7.Reverse"<<endl;
+        cout<<"8.Quit"<<endl;
+        cout<<"Enter your choice : ";
+        cin>>choice;
+        switch ( choice )
+        {
+        case 1:
+            cout<<"Enter the element: ";
+            cin>>element;
+            dl.create_list(element);
+            cout<<endl;
+            break;
+        case 2:
+            cout<<"Enter the element: ";
+            cin>>element;
+            dl.add_begin(element);
+            cout<<endl;
+            break;
+        case 3:
+            cout<<"Enter the element: ";
+            cin>>element;
+            cout<<"Insert Element after postion: ";
+            cin>>position;
+            dl.add_after(element, position);
+            cout<<endl;
+            break;
+        case 4:
+            if (dl.isEmpty())
+            {
+                cout<<"List empty,nothing to delete"<<endl;
+                break;
+            }
+            cout<<"Enter the element for deletion: ";
+            cin>>element;
+            dl.delete_element(element);
+            cout<<endl;
+            break;
+        case 5:
+            dl.display_dlist();
+            cout<<endl;
+            break;
+        case 6:
+            dl.count();
+            break;
+        case 7:
+            if (dl.isEmpty())
+            {
+                cout<<"List empty,nothing to reverse"<<endl;
+                break;
+            }
+            dl.reverse();
+            cout<<endl;
+            break;
+        case 8:
+            exit(1);
+        default:
+            cout<<"Wrong choice"<<endl;
+        }
+    }
+    return 0;
+}
