a7f8882ed05e4a3d177c7637271cb0bf6eceb448
danghai <danghai@pdx.edu>
2017-11-27 18:55:47 -0800

    noncanonical_input.c

---
+++ termios/noncanonical_input.c
@@ -0,0 +1,58 @@
+/*
+*     noncanonical_input.c: Read the /dev/tty in noncanonical mode
+*     sending 'z' to terminate the program, example how to use VMIN and VTIME
+*/
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+
+#define BAUDRATE B38400
+#define MODEMDEVICE "/dev/tty"
+#define _POSIX_SOURCE 1 /* POSIX compliant source */
+#define FALSE 0
+#define TRUE 1
+volatile int STOP=FALSE;
+
+int
+main(void){
+        int     fd,c, res;
+        struct  termios oldtio,newtio;
+        char    buf[255];
+
+        fd = open(MODEMDEVICE, O_RDWR | O_NOCTTY );
+        if (fd <0) {perror(MODEMDEVICE); exit(-1); }
+
+        tcgetattr(fd,&oldtio); /* save current port settings */
+
+        newtio.c_cflag = BAUDRATE | CRTSCTS | CS8 | CLOCAL | CREAD;
+        newtio.c_iflag = IGNPAR;
+        newtio.c_oflag = 0;
+
+        /* set input mode (non-canonical, no echo,...) */
+        newtio.c_lflag = 0;
+
+      //  newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
+      //  newtio.c_cc[VMIN]     = 5;   /* blocking read until 5 chars received */
+
+        newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
+        newtio.c_cc[VMIN]     = 0;   /* blocking read until 5 chars received */
+        tcflush(fd, TCIFLUSH);
+        tcsetattr(fd,TCSANOW,&newtio);
+
+
+        while (STOP==FALSE) {       /* loop for input */
+          res = read(fd,buf,255);   /* returns after 5 chars have been input */
+          buf[res]=0;               /* so we can printf... */
+          printf(":%s:%d\n", buf, res);
+          if (buf[0]=='z') STOP=TRUE;
+        }
+        tcsetattr(fd,TCSANOW,&oldtio);
+
+        return 0;
+}
